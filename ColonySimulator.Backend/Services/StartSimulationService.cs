a421c562a19f96f671d8495fb45f93e67c5f4928
Filip <filipkwiek888@gmail.com>
2024-05-27 22:41:21 +0200

    Add spectre.console lib and finish startup class

--- ColonySimulator.Backend/Services/StartSimulationService.cs
+++ ColonySimulator.Backend/Services/StartSimulationService.cs
@@ -1,5 +1,7 @@
 using ColonySimulator.Backend.Handlers.Interfaces.ProfessionsInterfaces;
 using ColonySimulator.Backend.Helpers;
+using ColonySimulator.Backend.Persistence;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ColonySimulator.Backend.Services;
@@ -13,6 +15,7 @@ public class StartSimulationService
     private readonly IServiceScopeFactory _serviceScopeFactory;
     private readonly PopCounter _counter;
     private readonly Year _year;
+    private readonly DataDisplayService _displayService;
     
     /// <summary>
     /// Constructor for this service
@@ -20,12 +23,14 @@ public class StartSimulationService
     /// <param name="serviceScopeFactory">Service scope factory</param>
     /// <param name="counter">Population counter class</param>
     /// <param name="year">current year in simulation</param>
+    /// <param name="displayService">Data display service</param>
     
-    public StartSimulationService(IServiceScopeFactory serviceScopeFactory, PopCounter counter, Year year)
+    public StartSimulationService(IServiceScopeFactory serviceScopeFactory, PopCounter counter, Year year, DataDisplayService displayService)
     {
         _serviceScopeFactory = serviceScopeFactory;
         _counter = counter;
         _year = year;
+        _displayService = displayService;
     }
     
     /// <summary>
@@ -37,19 +42,47 @@ public class StartSimulationService
     {
         using var serviceScope = _serviceScopeFactory.CreateScope();
         var profHandler = serviceScope.ServiceProvider.GetService<IProfessionHandler>();
+        var dbContext = serviceScope.ServiceProvider.GetService<ColonySimulatorContext>();
 
-        if (profHandler is not null)
+        if (profHandler is not null && dbContext is not null)
         {
             //for this case simulation will go till everyone in population is dead
+            //but for testing purposes, it will stop after 10 years
             for (;;)
             {
                 _year.YearOfSim++;
                 await profHandler.HandleApothecary();
                 await profHandler.HandleFarm();
 
+                if (_year.YearOfSim == 10)
+                {
+                    Console.WriteLine("Simulation end, specified period timed out! Showing data: ");
+                    
+                    var profOverview = new ProfessionsOverview
+                    {
+                        Apothecaries = await dbContext.Apothecaries.ToListAsync(ct),
+                        BlackSmiths = await dbContext.BlackSmiths.ToListAsync(ct),
+                        Timbers = await dbContext.Timbers.ToListAsync(ct),
+                        Traders = await dbContext.Traders.ToListAsync(ct),
+                        Medics = await dbContext.Medics.ToListAsync(ct),
+                        Farmers = await dbContext.Farmers.ToListAsync(ct),
+                    };
+        
+                    //change to some reliable values later
+                    var threatOverview = new ThreatsOverview
+                    {
+                        ThreatsDefeated = await dbContext.PlagueThreats.Where(x => x.Id % 2 == 0).ToListAsync(ct),
+                        ThreatsYieldedTo = await dbContext.PlagueThreats.Where(x => x.Id % 2 != 0).ToListAsync(ct),
+                    };
+                
+                    //Needs further improvement with new console lib in project
+                    Console.WriteLine(_displayService.SerializeAndDisplayData<ProfessionsOverview, ThreatsOverview>(profOverview, threatOverview));
+                    break;
+                }
+                
                 if (_counter.PopulationCount == 0)
                 {
-                    Console.WriteLine("Everyone's dead");
+                    Console.WriteLine("Everyone's dead, showing end data: ");
                     break;
                 }
             }
